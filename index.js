c90ade6a483eca5c25f7be1ba0584f432ac62eb9
Tanvir <toukir.developer.bd@gmail.com>
2023-05-03 12:24:06 +0600

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+// 3
+var cors = require('cors')
+const port = 5000
+
+// 3
+app.use(cors())
+// 2
+const chef = require('./data/chef.json')
+//4
+const recipe = require('./data/recipe.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// 2
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+//4
+app.get('/recipe', (req, res) => {
+    res.send(recipe)
+})
+
+//5
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    const singleRecipe = recipe.find((r) => r.chef_id == id)
+    // console.log(id);
+    console.log(singleRecipe);
+    res.send(singleRecipe)
+    // res.send(recipe)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
