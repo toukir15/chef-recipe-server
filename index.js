68c83019a4c4726240af1ad3011909f22833ef3a
Tanvir <toukir.developer.bd@gmail.com>
2023-05-04 18:28:31 +0600

    send chef data

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-// 3
-var cors = require('cors')
+
 const port = 5000
 
-// 3
-app.use(cors())
 
+// 2
+const chef = require('./data/chef.json')
 //4
 const recipe = require('./data/recipe.json')
 const review = require('./data/review.json')
@@ -15,7 +14,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-
+// 2
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
 //4
 app.get('/recipe', (req, res) => {
     res.send(recipe)
