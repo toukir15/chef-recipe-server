49165a42f0ed04b1f6ffa097f0176a7a8144c09e
Tanvir <toukir.developer.bd@gmail.com>
2023-05-05 17:17:56 +0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
-// 3
+
 var cors = require('cors')
 const port = 5000
 
-// 3
+
 app.use(cors())
-// 2
+
+// data 
 const chef = require('./data/chef.json')
-//4
 const recipe = require('./data/recipe.json')
 const review = require('./data/review.json')
 
@@ -16,11 +16,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-// 2
 app.get('/chef', (req, res) => {
     res.send(chef)
 })
-//4
+
 app.get('/recipe', (req, res) => {
     res.send(recipe)
 })
@@ -29,7 +28,6 @@ app.get("/review", (req, res) => {
     res.send(review)
 })
 
-//5
 app.get('/recipe/:id', (req, res) => {
     const id = req.params.id;
     const singleRecipe = recipe.find((r) => r.chef_id == id)
